0396b0d28e1f2ad0ee5b3ed5659e8c8136b4304b
Vinicius Cossio de Oliveira <vinicius.oliveira@rsinet.com.br>
2022-02-07 22:58:03 -0300

    feat: standard config deleted

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/LoginStyle.css';
+
+function LoginPage(){
+    return(
+        <>
+        <title>Login</title>
+        <header className = "loginPage">
+            <div className = "img-Bckgd">BACKGROUND IMAGE</div>
+            <div className = "container-login">LOGIN</div>
+        </header>
+        </>
+    )
+}
+
+export default LoginPage;
